962c1990c14607fb391e0ca01e1d14bad9817d6f
Armand-Cezar Mathe <me@cezarmathe.com>
2023-04-26 07:56:17 +0300

    work on time and tokio in bgworker

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 // src/lib.rs
 
 mod com;
+mod time;
 mod worker;
 
-use chrono::DateTime;
-use chrono::NaiveDateTime;
 use chrono::Utc;
 
 use pgrx::PgLwLock;
@@ -25,11 +24,6 @@ pub extern "C" fn _PG_init() {
     worker::init();
 }
 
-// // (January 1, 2000, UTC) - (January 1, 1970, UTC) as seconds;
-// const PG_EPOCH_SECS: u128 = 946_708_560;
-// // (January 1, 2000, UTC) - (January 1, 1970, UTC) as microseconds;
-// const PG_EPOCH_MICROS: u128 = PG_EPOCH_SECS * 1_000_000;
-
 #[pg_trigger]
 fn on_horloge_timers_event<'a>(trigger: &'a PgTrigger<'a>) -> Result<
     Option<PgHeapTuple<'a, impl WhoAllocated>>,
@@ -37,22 +31,20 @@ fn on_horloge_timers_event<'a>(trigger: &'a PgTrigger<'a>) -> Result<
 > {
     let now = Utc::now();
 
-    let id = trigger.new().unwrap().get_by_name::<i64>("id").unwrap().unwrap();
+    if !trigger.event().fired_by_insert(){
+        error!("trigger fired by something other than insert");
+    }
+
+    let _id = trigger.new().unwrap().get_by_name::<i64>("id").unwrap().unwrap();
     let ts_pg = trigger.new().unwrap().get_by_name::<Timestamp>("ts").unwrap().unwrap();
 
-    let ts_i64 = Into::<i64>::into(ts_pg);
-    let ts = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp_micros(ts_i64).unwrap(), Utc);
+    let ts = time::pg_to_chrono(ts_pg);
 
-    // let ts_i64 = Into::<i64>::into(ts);
-    // let ts_u64 = u64::try_from(Into::<i64>::into(ts)).unwrap();
-    // let ts_u128 = u128::from(ts_u64) + PG_EPOCH_MICROS;
-    // let now = std::time::UNIX_EPOCH.elapsed().unwrap().as_micros();
-
-    panic!("ts={}, now={}", ts.timestamp_micros(), now.timestamp_micros());
-
-    if now >= ts_u128 {
-        error!("timer is in the past: now={}, new.ts={}", now, ts_u128);
+    if now >= ts {
+        error!("timer is in the past: now={}, new.ts={}", now, ts);
     }
+
+    // do something with the timer
 
     Ok(Some(trigger.new().or(trigger.old()).expect("neither \"new\" nor \"old\"")))
 }
--- src/worker.rs
+++ src/worker.rs
@@ -4,23 +4,20 @@ use pgrx::bgworkers::*;
 use pgrx::log;
 use pgrx::prelude::*;
 
+use tokio::time;
+use tokio::time::MissedTickBehavior;
+
 use std::time::Duration;
 
 const WORKERS_COUNT: i32 = 1;
 
 pub(crate) fn init() {
     for i in 0..WORKERS_COUNT {
-        // create a new worker
         BackgroundWorkerBuilder::new(format!("horloge-{}", i).as_str())
-            // set the function to run
             .set_function("horloge_worker_main")
-            // set the arg to the worker id
-            .set_argument(i.into_datum())
-            // set the library to load
+            .set_argument(i.into_datum()) // worker ID
             .set_library("horloge")
-            // enable SPI access
             .enable_spi_access()
-            // load the worker
             .load();
     }
 }
@@ -29,10 +26,18 @@ pub(crate) fn init() {
 #[no_mangle]
 pub extern "C" fn horloge_worker_main(arg: pg_sys::Datum) {
     let worker_id = unsafe { i32::from_datum(arg, false) }.unwrap();
-    self::run_worker(worker_id);
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        // .event_interval(42)
+        .max_blocking_threads(1)
+        .build()
+        .unwrap();
+
+    runtime.block_on(self::run_worker(worker_id));
 }
 
-fn run_worker(worker_id: i32) {
+async fn run_worker(worker_id: i32) {
     // these are the signals we want to receive.  If we don't attach the SIGTERM handler, then
     // we'll never be able to exit via an external notification
     BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGHUP | SignalWakeFlags::SIGTERM);
@@ -46,33 +51,53 @@ fn run_worker(worker_id: i32) {
         worker_id,
     );
 
-    // wake up every 10s or if we received a SIGTERM
-    while BackgroundWorker::wait_latch(Some(Duration::from_secs(1))) {
+    let mut poll_term_interval = time::interval(Duration::from_secs(1));
+    poll_term_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+    let mut poll_timers_interval = time::interval(Duration::from_millis(1));
+    poll_timers_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+    let on_poll_term_inverval = || {
+        if BackgroundWorker::sigterm_received() {
+            return false;
+        }
+
         if BackgroundWorker::sighup_received() {
             // on SIGHUP, you might want to reload some external configuration or something
         }
 
+        return true;
+    };
+
+    let on_poll_timers_interval = || {
         // within a transaction, execute an SQL statement, and log its results
         BackgroundWorker::transaction(|| {
             Spi::connect(|client| {
-                let tuple_table = client
-                    .select(
-                    "select localtimestamp",
-                    None,
-                    None,
-                    )
-                    .unwrap();
+                let tuple_table = client.select("select localtimestamp", None, None).unwrap();
                 tuple_table.for_each(|tuple| {
-                    let ts= tuple
+                    let ts = tuple
                         .get_datum_by_ordinal(1)
                         .unwrap()
                         .value::<Timestamp>()
                         .unwrap()
                         .unwrap();
-                    log!("Tick: ({})", Into::<i64>::into(ts));
+                    // log!("Tick: ({})", Into::<i64>::into(ts));
                 });
             });
         });
+    };
+
+    loop {
+        tokio::select! {
+            _ = poll_term_interval.tick() => {
+                if !on_poll_term_inverval() {
+                    break;
+                }
+            }
+            _ = poll_timers_interval.tick() => {
+                on_poll_timers_interval();
+            }
+        }
     }
 
     log!(
