1b0cd7cfb84462989337a56912a2e78e8086bf6c
Armand-Cezar Mathe <me@cezarmathe.com>
2023-04-29 11:57:20 +0300

    Add docs, Dered and DerefMut impls to Queue

--- src/queue.rs
+++ src/queue.rs
@@ -2,12 +2,32 @@
 
 use pgrx::PGRXSharedMemory;
 
-pub struct MpMcQueue<T, const N: usize>(pub heapless::mpmc::MpMcQueue<T, N>);
+use std::ops::Deref;
+use std::ops::DerefMut;
+
+// MpMpQueue is a wrapper around heapless::mpmc::MpMcQueue that implements
+// PGRXSharedMemory. This allows us to use it as a static variable in a
+// PgLwLock.
+pub struct MpMcQueue<T, const N: usize>(heapless::mpmc::MpMcQueue<T, N>);
 
 unsafe impl<T, const N: usize> PGRXSharedMemory for MpMcQueue<T, N> {}
 
 impl<T, const N: usize> Default for MpMcQueue<T, N> {
     fn default() -> Self {
         Self(heapless::mpmc::MpMcQueue::default())
+    }
+}
+
+impl<T, const N: usize> Deref for MpMcQueue<T, N> {
+    type Target = heapless::mpmc::MpMcQueue<T, N>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T, const N: usize> DerefMut for MpMcQueue<T, N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
